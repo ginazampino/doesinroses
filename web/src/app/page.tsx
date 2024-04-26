3a4be21e47bda9a400ee488b8b7398d3883fa81c
Gina <9094115+ginazampino@users.noreply.github.com>
2024-04-25 21:41:09 -0400

    Testing next-sanity

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,24 @@
-import Image from 'next/image';
 import styles from './page.module.css';
+import { client } from '../../src/utils/client';
 
-export default function Home() {
-    return <main>Homepage</main>;
+type Post = {
+    _id: string;
+    title?: string;
+    slug?: {
+        current: string;
+    };
+};
+
+export default async function Home() {
+    const posts = await client.fetch<Post[]>(`*[_type == "post"]`);
+
+    return (
+        <ul>
+            {posts.map((post) => (
+                <li key={post._id}>
+                    <a href={post?.slug?.current}>{post?.title}</a>
+                </li>
+            ))}
+        </ul>
+    );
 }
